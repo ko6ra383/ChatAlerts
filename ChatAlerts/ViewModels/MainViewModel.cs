1d7081873b2464554663e63732d56c09036d6215
Kobra Matt <matveiamelhcenko@mail.ru>
2022-05-29 16:55:27 +0700

    Основная модель-представления

---
+++ ChatAlerts/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatAlerts.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        #region Свойства
+
+        #endregion
+        #region Команды
+        #endregion
+        public MainViewModel()
+        {
+            #region Команды
+
+            #endregion
+        }
+    }
+}
