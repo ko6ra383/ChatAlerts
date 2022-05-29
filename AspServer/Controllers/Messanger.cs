90e9c64f171e8198d088a93b6fde6086d7996e9e
Kobra Matt <matveiamelhcenko@mail.ru>
2022-05-30 00:02:30 +0700

    создание asp net core приложения с контролером обработки сообщений

---
+++ AspServer/Controllers/Messanger.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System;
+namespace AspServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Messanger : ControllerBase
+    {
+        static List<Message> ListOfMessages = new List<Message>();
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            string output = "Not found";
+            if ((id < ListOfMessages.Count) && (id >= 0))
+            {
+                output = JsonConvert.SerializeObject(ListOfMessages[id]);
+            }
+            Console.WriteLine($"Зпрошено сообщение N{id} : {output}");
+            return output;
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage([FromBody] Message msg)
+        {
+            if (msg == null)
+            {
+                return BadRequest();
+            }
+            ListOfMessages.Add(msg);
+            Console.WriteLine($"Всего сообщений: {ListOfMessages.Count} Посланное сообщение {msg}");
+            return new OkResult();
+        }
+    }
+}
