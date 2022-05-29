e637dc0c1337b60a54a7256514fd8e141fff716f
Kobra Matt <matveiamelhcenko@mail.ru>
2022-05-29 17:01:51 +0700

    Добавления класса упрощающего создание комманд

---
+++ ChatAlerts/Infrastructure/Commands/LambdaCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ChatAlerts.Infrastructure.Commands
+{
+    public class LambdaCommand : BaseCommand
+    {
+        private readonly Action<object> _Execute;
+        private readonly Func<object, bool> _CanExecute;
+        public LambdaCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        public override bool CanExecute(object? parameter) => _CanExecute?.Invoke(parameter) ?? true;
+        public override void Execute(object? parameter) => _Execute(parameter);
+    }
+}
